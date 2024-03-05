368095703c4201defe56ce8d872252801ff15921
harshad <codewithharshad@gmail.com>
2024-03-06 00:31:04 +0530

    update

--- vue-landing-page/src/language/en_US.ts
+++ vue-landing-page/src/language/en_US.ts
@@ -9,7 +9,7 @@ export default {
     },
     //footer
     footer: {
-        para: 'ONE-BALL. All rights reserved'
+        para: ' '
     },
     //home
     home: {
@@ -71,142 +71,14 @@ export default {
             h1: 'Join Us in ',
             h2: 'Shaping the Future',
             para: "AI Technology invites you to be a part of a transformative journey into the future of AI and technology. Whether you're a business seeking innovative solutions or an individual looking to stay ahead in the digital age, we have the expertise and resources to propel you forward.",
-            invest: 'INVEST NOW'
+            invest: ' JOIN NOW'
         },
        contact:{
          h1:'',
-         invest: 'INVEST NOW',
+         invest: ' JOIN NOW',
          contact: 'CONTACT',
          para: 'Explore the possibilities with AI Technology. Contact us today to discover how our trusted AI and tech solutions can elevate your business to new heights.'
        }
 
     },
-    //About US
-    about: {
-        hero: {
-            heading: 'About Company',
-            cta:'',
-        },
-        about: {
-            h1: 'Welcome to',
-            h2: 'AI Technology',
-            para: 'Where innovation meets influence. As a trailblazer in the realms of international computer science and artificial intelligence, we have left an indelible mark on prestigious platforms such as KDD, CVPR, ICCV, ACM, IEEE, and more.',
-            c1: 'Gave insurances',
-            c2: 'Professional team',
-            c3: 'Satisfied customers',
-            c4: 'Our success rate',
-        },
-        achieve: {
-            heading: 'Our Achievements',
-            h1: 'Influence Across Fields: ',
-            p1: 'AI Technology holds outstanding influence across diverse fields within computer science and artificial intelligence. Our presence in renowned conferences and organizations reflects our commitment to pushing the boundaries of technological innovation.',
-            h2:'Pioneering Solutions:',
-            p2: "We take pride in pioneering smart, sustainable, and creative solutions. At AI Technology, we don't just follow trends; we set them. Our team is dedicated to developing groundbreaking solutions that define the future of AI.",
-            h3: 'Shaping a Smarter Future: ',
-            p3:'Our mission extends beyond the present; we are actively shaping a smarter and more sustainable AI future. Through our work, we aspire to contribute to a world where artificial intelligence not only meets the demands of today but anticipates the challenges of tomorrow.',
-        },
-        key:{
-            h1: 'Key Milestones',
-            p1:"Our commitment to innovation is evident in the 10+ technology patents we've secured. These patents represent our dedication to creating solutions that are not only effective but also unique in the ever-evolving landscape of technology.",
-            p2: "AI Technology is not just a player; we're an investment in the future. With over 20 capital investments, we've garnered support from those who believe in the transformative potential of our initiatives.",
-            para: "At AI Technology, we invite you to be a part of our journey as we continue to lead, innovate, and shape the future of AI. Our achievements are a testament to our commitment to excellence, and we look forward to pushing the boundaries of what's possible in the world of technology.",
-
-        }
-    },
-    //team  
-    team: {
-        heading: 'Our Dedicated',
-        heading2: 'Team of Experts',
-        card: {
-         c1:{
-            p: 'Stéphane has worked across various departments in the financial services sector at companies like Paris Bank, CapitalOne, and AIG in France, the United Arab Emirates, and Spain. Technology Officer at Rayan walaa. He has worked across various departments in the financial services sector at companies like Paris Bank, CapitalOne, and AIG in France, the United Arab Emirates, and Spain.',
-            name : 'Stéphane Flaquet',
-            desc : 'CHIEF OPERATING AND TECHNOLOGY OFFICER'
-         },
-         c2:{
-            p: 'Kate joined Hiscox in June 2012, leading a team of over 200 people spread across three locations, responsible for sales, marketing, operations, and underwriting. In 2023, she became the CEO of the newly established Rayanwalaa. She previously spent 12 years at Vodafone, where as Director of Vodafone International Enterprise, she identified a significant new opportunity to serve global multinational clients and established effective operations to support the joint venture. Kate began her career at Andersen Consulting.',
-            name : 'Kate Markham',
-            desc : 'CHIEF EXECUTIVE OFFICER (CEO)'
-         },
-         c3:{
-            p: 'Emily Clarke, our chief financial officer (CFO), brings seven years of venture capital management experience to the table. Specializing in strategic planning and business model design, Tom plays a crucial role in advancing our offerings in AI, insurance, and technology.',
-            name : 'Emily Clarke',
-            desc : 'Chief Financial Officer (CFO)'
-         },
-         c4:{
-            p: 'Michael Johnson joined Rayanwalaa in November 2023 as the Group Chief Risk Officer. With 12 years of experience, Michael has held positions in both the UK and the USA. He later served as the Chief Risk Officer (CRO) at International General SaaS, overseeing risk management functions in over 80 regions outside the USA. Prior to that, he spent ten years at AXA SaaS in the UK, rising to the position of Chief Actuary.',
-            name : 'Michael Johnson',
-            desc : 'Chief Financial Officer (CFO)'
-         },
-        }
-    },
-    //ai
-    ins:{
-        hero:{
-            h1: 'AI SaaS',
-            h2: 'Application Sector',
-            p:'With low premiums, high protection, and rich product lines',
-            cta: ''
-        },
-        about:{
-            h1: 'Overview of',
-            h2: 'AI SaaS',
-            p: "Welcome to the innovative realm of AI SaaS, where we redefine the game with low premiums, robust protection, and an extensive array of products. Our insurance application sector is not just about shielding your investments; it's about elevating your experience with cutting-edge features and unbeatable advantages."
-        },
-        type:{
-            h1: 'Types of',
-            h2: 'AI SaaS',
-            p:'AI SaaS introduces a tiered system, turning insurance into a personalized experience. The higher your tier, the greater your claims settlement ratio. Starting at a solid 55%, our basic user compensation can soar to an impressive 94%.',
-            c1: 'Premium',
-            c11:"Insure first and then charge, and charge 30% of the profit as insurance premium to ensure stable income.",
-            c2: 'Loss',
-            c22: 'Compensation will be paid if you lose or lose money in a game you participated in using an auxiliary device.',
-        },
-        comp:{
-            h1: 'Compensation',
-            h2 : 'Up to 94%',
-            p1: 'The higher level of compensation, the higher will be the claim settlement ratio.',
-            p2: 'Basic users claims are 55%',
-            cta: 'Want to contact with us?',
-        },
-        foot:{
-            h1: 'Football insurance',
-            h2 : 'Reverse Score Method',
-            p: 'The bet is on a score that is different from the result of the football match. If the score you buy is different from the score of the match result, it will be judged as a profit for you.', 
-        },
-        tech:{
-            h1: 'Technical advantages',
-            h2 : 'Programmers & Analysts',
-            p1:"Automatic follow-up program to ensure safe and stable growth of fans and players' funds; professional event analyst team, more than 130 industry data analysts.",
-            p2: "All competitions are fair, just and open events such as the five major leagues and international competitions."
-        }
-    },
-      
-    //AI 
-    ai: {
-        hero:{
-            h1: 'Welcome to AI',
-            h2: 'Artificial Intelligence',
-            p1:"The World's Leading Decentralized Artificial Intelligence Market",
-        },
-        tech:{
-            h1: 'Technology Platform: ',
-            h2: 'Artificial Intelligence Service Network',
-            p:"Our cutting-edge technology platform, the Artificial Intelligence Service Network, empowers you with the capability to access inference or model training across multiple domains. Whether it's image processing, video analysis, speech recognition, text understanding, time series predictions, bio artificial intelligence, or network analysis, AI AI has got you covered.",
-        },
-        all:{
-            h1  :'All professional',
-            h2  :'AI services',
-            c1: 'Virtual Avatar',
-            c2: 'Virtual Assistant',
-            c3: 'Virtual Guide',
-            c4: 'Virtual Doctor',
-            c5: 'Virtual Teacher',
-            h3: 'Our AI Partners',
-            p1: 'RAYAN WALAA AI is proud to collaborate with leading AI partners. Together, we aim to push the boundaries of what artificial intelligence can achieve. Meet our esteemed partners who contribute to the success and innovation of RAYAN WALAA AI.',
-            p2:'Explore our AI partners and their contributions.',
-            p3:"Join us on the forefront of decentralized artificial intelligence. RAYAN WALAA AI is not just a platform; it's a revolution shaping the future of AI technology.",
-        }
-        
-    }
 };
